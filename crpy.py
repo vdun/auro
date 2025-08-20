316aa1367f15864b1fb1880d3af88795b097575b
vdun <vdun@users.noreply.github.com>
2025-08-20 14:01:16 +0200

    Update crpy.py

--- crpy.py
+++ crpy.py
@@ -1,6 +1,11 @@
+import time
 from javascript import require
 tv=require('@mathieuc/tradingview')
-print(tv)
+# print(tv)
+c=tv.Client(); time.sleep(1)
+ch = c.Session.Chart()
+print(ch)
+	
 # with open('hello.txt', 'w') as f:
 # 	f.write('Hello World from Python script!')
 # print("File written successfully")
