fe1e240469284dd6c986b83bb43cfdce429f99a1
vdun <vdun@users.noreply.github.com>
2025-08-20 13:54:28 +0200

    Update crpy.py

--- crpy.py
+++ crpy.py
@@ -1,3 +1,6 @@
-with open('hello.txt', 'w') as f:
-	f.write('Hello World from Python script!')
-print("File written successfully")
+from javascript import require
+tv=require('@mathieuc/tradingview')
+print(tv)
+# with open('hello.txt', 'w') as f:
+# 	f.write('Hello World from Python script!')
+# print("File written successfully")
