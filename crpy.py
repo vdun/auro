128c69324b5ea43c1c5be27b3f6f68169b36dbf1
vdun <vdun@users.noreply.github.com>
2025-08-20 14:09:46 +0200

    Update crpy.py

--- crpy.py
+++ crpy.py
@@ -1,4 +1,4 @@
-import time, random
+import time, random, re
 from javascript import require
 lCh=[]; lSt=[];
 tv=require('@mathieuc/tradingview')
@@ -20,6 +20,14 @@ for i1 in range(0, len(lSt) - 1, 2):
 	ch.setMarket('BINANCE:BTCUSDT', { 'timeframe': tf }); time.sleep(1)
 	z =tv.getIndicator(std, 'last'); z2=tv.getIndicator(std2, 'last'); time.sleep(1)
 print([z.description, z2.description])
+
+if (i in [1]) and ( re.findall(r'(?i)ZigZag\+\+', z.description+' '+z2.description) ):
+	if int(vv['New_Higher_Low']) ==1: arB.append({'c': cc, 's': 'zz_hl', 'd':'long'})
+	if int(vv['New_Lower_Low'])  ==1: arB.append({'c': cc, 's': 'zz_ll', 'd':'long'})
+	if int(vv['New_Higher_High'])==1: arB.append({'c': cc, 's': 'zz_hh', 'd':'short'})
+	if int(vv['New_Lower_High']) ==1: arB.append({'c': cc, 's': 'zz_lh', 'd':'short'})
+for i, r in pd.DataFrame(arB).drop_duplicates().iterrows():
+	print(f"- {r['c']}, {r['s']}, {r['d']}, {datetime.now()}")
 # with open('hello.txt', 'w') as f:
 # 	f.write('Hello World from Python script!')
 # print("File written successfully")
